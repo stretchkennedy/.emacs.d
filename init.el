ce6097019344d07c1218268f367559fc181af95e
Tim Kennedy <t.m.knndy@gmail.com>
2015-05-06 19:58:00 +1000

    Got rid of the annoying magit warning.

--- init.el
+++ init.el
@@ -101,6 +101,7 @@
 (require 'magit)
 (global-set-key (kbd "M-g s") 'magit-status)
 (global-set-key (kbd "M-g b") 'magit-blame-mode)
+(setq magit-last-seen-setup-instructions "1.4.0")(setq magit-last-seen-setup-instructions "1.4.0") ; bugger off
 
 ;; RVM
 (require 'rvm)
