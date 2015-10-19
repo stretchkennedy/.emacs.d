5f1ecf01e504e3b6a35c6518c2a06c578f48cdef
Tim Kennedy <tim.kennedy@tickbox.com.au>
2015-10-19 16:40:47 +1100

    Updated libraries

--- elpa/elixir-mode-20150826.1305/elixir-mode-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "elixir-mode" "20150826.1305" "Major mode for editing Elixir files"
-  '((emacs "24")
-    (pkg-info "0.4"))
-  :url "https://github.com/elixir-lang/emacs-elixir" :keywords
-  '("languages" "elixir"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/s-20150825.50/s-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; s-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("s.el") (21989 9850 237617 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; s-autoloads.el ends here
