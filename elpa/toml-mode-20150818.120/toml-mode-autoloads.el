30e2f0f4594571830068d99c50c370b91f68d8f8
Tim Kennedy <t.m.knndy@gmail.com>
2016-03-17 22:35:56 +1100

    Add toml-mode

---
+++ elpa/toml-mode-20150818.120/toml-mode-autoloads.el
@@ -0,0 +1,24 @@
+;;; toml-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "toml-mode" "toml-mode.el" (22250 38532 662376
+;;;;;;  457000))
+;;; Generated autoloads from toml-mode.el
+
+(autoload 'toml-mode "toml-mode" "\
+
+
+\(fn)" t nil)
+
+(add-to-list 'auto-mode-alist '("\\.toml$" . toml-mode))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; toml-mode-autoloads.el ends here
