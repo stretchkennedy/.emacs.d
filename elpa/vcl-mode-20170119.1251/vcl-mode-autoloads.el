428b5d106a9434ac7227d1815510fe0b6ce83108
Tim Kennedy <tim.kennedy@tickbox.com.au>
2017-09-19 11:30:53 +1000

    Install vcl-mode

---
+++ elpa/vcl-mode-20170119.1251/vcl-mode-autoloads.el
@@ -0,0 +1,21 @@
+;;; vcl-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "vcl-mode" "vcl-mode.el" (22976 29364 0 0))
+;;; Generated autoloads from vcl-mode.el
+
+(autoload 'vcl-mode "vcl-mode" "\
+Mode for Varnish Command Language
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; vcl-mode-autoloads.el ends here
---
+++ elpa/vcl-mode-20170119.1251/vcl-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "vcl-mode" "20170119.1251" "Syntax highlighting for Varnish Command Language" 'nil :commit "3d86c1352a7370d558d25f4c8f7be744e7d27332")
