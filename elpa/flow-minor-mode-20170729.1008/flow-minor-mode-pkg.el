3348756f7422812e88246a55b21735259e258652
Tim Kennedy <t.m.knndy@gmail.com>
2018-09-23 16:10:47 +1000

    Upgrade packages

--- elpa/flow-minor-mode-20170729.1008/flow-minor-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flow-minor-mode" "20170729.1008" "Flow type mode based on web-mode." '((emacs "25.1")) :commit "75067baceed7d699dda43944da49dc58d22baeff" :url "https://github.com/an-sh/flow-minor-mode")
---
+++ elpa/vcl-mode-20170119.2051/vcl-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "vcl-mode" "20170119.2051" "Syntax highlighting for Varnish Command Language" 'nil :commit "3d86c1352a7370d558d25f4c8f7be744e7d27332" :authors '(("Stig Sandbeck Mathisen" . "ssm@redpill-linpro.com")) :maintainer '("Stig Sandbeck Mathisen" . "ssm@redpill-linpro.com"))
