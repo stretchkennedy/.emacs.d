5121fe3ae241adaacc31776b224a12da3d7ab3e9
Tim Kennedy <t.m.knndy@gmail.com>
2018-11-18 16:47:56 -0800

    Typescript + updates

---
+++ elpa/lsp-go-20180914.515/lsp-go-autoloads.el
@@ -0,0 +1,15 @@
+;;; lsp-go-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("lsp-go.el") (23538 1352 430472 163000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; lsp-go-autoloads.el ends here
