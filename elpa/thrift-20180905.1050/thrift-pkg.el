654da8704c7768c496d1ef5a8e1ffb07002d0fb1
Tim Kennedy <tkennedy@uber.com>
2018-12-12 12:31:09 -0800

    Add thrift-mode

---
+++ elpa/thrift-20180905.1050/thrift-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "thrift" "20180905.1050" "major mode for fbthrift and Apache Thrift files" '((emacs "24")) :commit "511b6abc140113faeeabb7cc6e8fd6682e515437" :keywords '("languages"))
